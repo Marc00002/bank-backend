af270d2924dcc2e2f90c079a26f0d3b41e006383
Marc00002 <122208252+Marc00002@users.noreply.github.com>
2025-06-03 12:15:58 +0200

    Fix CORS for Vercel frontend

--- server.js
+++ server.js
@@ -7,7 +7,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: 'https://bank-frontend-nine.vercel.app/',
+  credentials: true
+}));
 app.use(express.json());
 
 // Connect to MongoDB
